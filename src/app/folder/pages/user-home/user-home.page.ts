b40fd790970a9527134eca7c62e43f02ebc1a61b
Saeed Arshad <m.saeedarshad95@gmail.com>
2020-03-01 16:43:34 +0500

    Start Scan redirect to scan qrcode page

--- src/app/folder/pages/user-home/user-home.page.ts
+++ src/app/folder/pages/user-home/user-home.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-user-home',
-  templateUrl: './user-home.page.html',
-  styleUrls: ['./user-home.page.scss'],
+    selector: 'app-user-home',
+    templateUrl: './user-home.page.html',
+    styleUrls: ['./user-home.page.scss'],
 })
 export class UserHomePage implements OnInit {
+    rewardPoints = 0;
 
-  constructor() { }
+    constructor(private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // Api call to get Score
+        this.rewardPoints = 25;
+    }
+
+    openScanPage() {
+        this.router.navigate(['folder/pages/qrcode-scan']);
+    }
 
 }
