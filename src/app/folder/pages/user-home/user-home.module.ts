4c6f22d555fbab2fe8d540916218b87788e94cff
Saeed Arshad <m.saeedarshad95@gmail.com>
2020-03-01 15:31:12 +0500

    add user-home page

---
+++ src/app/folder/pages/user-home/user-home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { UserHomePageRoutingModule } from './user-home-routing.module';
+
+import { UserHomePage } from './user-home.page';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        UserHomePageRoutingModule
+    ],
+    exports: [
+        UserHomePage
+    ],
+    declarations: [UserHomePage]
+})
+export class UserHomePageModule {}
